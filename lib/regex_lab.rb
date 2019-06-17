292a08bbd7b01dbe374e4f674e9e2f064a7fc32a
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-17 11:38:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,8 +16,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/[A-Z]+\w+[\.!?]\b/) then
-    true
+  if text.match(/^[A-Z].+[\.!?]$/) then
+     true
   else false
   end
   
